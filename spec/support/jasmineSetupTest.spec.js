5c6da6cde1e307f0e2e07a2b1b31c33c568478dc
aaronRhodebeck <arhodebeck@gmail.com>
2017-10-31 12:06:07 -0500

    Add dependencies and setup react, babel, and webpack

---
+++ spec/support/jasmineSetupTest.spec.js
@@ -0,0 +1,8 @@
+// describe("Jasmine test", () => {
+//   it("should have passing test", () => expect(true).toBe(true));
+//   it("should have a failing test", () => expect(true).toBe(false));
+//   it("should have a pending test", () => {
+//     pending("This test is marked as pending");
+//     expect(true).toBe(true);
+//   });
+// });
